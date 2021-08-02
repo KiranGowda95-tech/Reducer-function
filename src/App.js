214e9c20a69efa9ab375e93f37defa26169ade7f
KiranGowda95-tech <76085458+KiranGowda95-tech@users.noreply.github.com>
2021-08-02 09:37:22 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -2,40 +2,7 @@ import "./styles.css";
 import React, { useState, useReducer } from "react";
 import Modal from "./Modal";
 import data from "./data";
-
-const reducer = (state, action) => {
-  console.log(state);
-  if (action.type === "ADD_ITEM") {
-    const newPeople = [...state.people, action.payload];
-    return {
-      ...state,
-      people: newPeople,
-      isModalOpen: true,
-      modalContent: "Item added"
-    };
-  }
-  if (action.type === "NO_VALUE") {
-    return {
-      ...state,
-      isModalOpen: true,
-      modalContent: "please enter the value"
-    };
-  }
-
-  if (action.type === "CLOSE_MODAL") {
-    return { ...state, isModalOpen: false };
-  }
-
-  if (action.type === "REMOVE_ITEM") {
-    const newPeople = state.people.filter(
-      (person) => person.id !== action.payload
-    );
-    return { ...state, people: newPeople };
-  }
-
-  throw new Error("no matching found");
-  //return state;
-};
+import { reducer } from "./Reducer";
 
 const defaultState = {
   people: [],
